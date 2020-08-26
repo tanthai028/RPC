5311aaff3b01677bb9396eb1bcaced2fbf94ebf3
Tan <35444054+tanthai028@users.noreply.github.com>
2020-08-26 04:21:33 -0400

    Update rpc.py

--- rpc.py
+++ rpc.py
@@ -1,10 +1,7 @@
 from random import choice
-from time import sleep
-from sys import stdout
 from os import system
 import emoji
 system('clear')
-
 
 def main():
 
@@ -19,74 +16,67 @@ def main():
   rock = emoji.emojize(':oncoming_fist:')
   paper = emoji.emojize(':hand_with_fingers_splayed:')
   scissors = emoji.emojize(':victory_hand:')
-  
 
+  global wins
+  global loses
   wins, loses, = 0, 0
 
-  while (wins or loses) != 2:
+  while (wins + loses) != 3:
 
     yourChoice = ''
-    while yourChoice not in ['1','2','3']:
 
-      yourChoice = input(f'''
- {yourEmj}  vs {oppEmj}
+    interface(yourEmj, oppEmj)
+    yourChoice = input('\n> ')
 
-1. Rock
-2. Paper
-3. Scissors
+    print()
+    print('-' * 20)
+    print()
 
-> ''')
-      print()
-      print('-' * 20)
-      print()
+    oppChoice = choice(rpc)
 
-      oppChoice = choice(rpc)
+    if oppChoice == 'rock':
+      oppEmj = rock
+    elif oppChoice == 'paper':
+      oppEmj = paper
+    elif oppChoice == 'scissors':
+      oppEmj = scissors
 
-      if oppChoice == 'rock':
-        oppEmj = rock
-      elif oppChoice == 'paper':
-        oppEmj = paper
-      elif oppChoice == 'scissors':
-        oppEmj = scissors
+    if yourChoice == 'rock':
+      yourEmj = rock
+    elif yourChoice == 'paper':
+      yourEmj = paper
+    elif yourChoice == 'scissors':
+      yourEmj = scissors
 
-      if yourChoice == '1':
-        yourEmj = rock
-      elif yourChoice == '2':
-        yourEmj = paper
-      elif yourChoice == '3':
-        yourEmj = scissors
+    if yourChoice == 'rock' and oppChoice == 'scissors' or yourChoice == 'paper' and oppChoice == 'rock' or yourChoice == 'scissors' and oppChoice == 'paper': 
+      wins += 1
+      print('You Win!')
 
-      if yourChoice == '1' and oppChoice == 'scissors' or yourChoice == '2' and oppChoice == 'rock' or yourChoice == '3' and oppChoice == 'paper': 
-        wins += 1
-        print('You Win!')
+    elif yourChoice == oppChoice:
+      print('You Draw!')
 
-      elif rpc[int(yourChoice) - 1] == oppChoice:
-        print('You Draw!')
-
-      else:
-        loses += 1
-        print('You Lose')
+    else:
+      loses += 1
+      print('You Lose')
+    
+    
       
   
   if wins > loses:
-    print(f'''
-  {yourEmj}  vs {oppEmj}
-
-1. Rock
-2. Paper
-3. Scissors''')
+    interface(yourEmj, oppEmj)
     print('\nYou Won!')
     
   else:
-    print(f'''
-  {yourEmj}  vs {oppEmj}
-
-1. Rock
-2. Paper
-3. Scissors''')
+    interface(yourEmj, oppEmj)
     print('\nYou Lost!')
-  
- 
 
+def interface(yourEmj, oppEmj):
+  global wins
+  global loses
+
+  print(f'Score: {wins}:{loses}')
+  print(f'\n{yourEmj}  vs {oppEmj}')
+  
 main()
+
 
